3ccf64bf7ed45187d11e2967ecbddb3c0a134e20
Benjamn <bfreeman@lcs.on.ca>
2021-01-31 16:59:41 -0500

    incorrect data type function

--- PerimeterAreaVoulumeHelper/main.swift
+++ PerimeterAreaVoulumeHelper/main.swift
@@ -7,7 +7,64 @@
 
 import Foundation
 
+// MARK: Input
+
+
+print("Surface Area Calculator")
+print("```````````````````````")
+print(",,,,,,,,,,,,,,,,,,,,,,,", terminator: "")
+print("Type the shape you would like to calculate", terminator: "")
+print("""
+      Cylinder: 1
+
+
+    
+    
+    
+    
+    
+    
+    
+    """)
+
+var shapeInput = readLine()!
+
+var cylinderRadius = 0.0
+var cylinderHight = 0.0
+
+switch shapeInput {
+case "1":
+    print("What is the radius")
+    
+var cylinderRadius = readLine()!
+    
+    print("What is the hight")
+    
+var cylinderHight = readLine()!
+    
+    let puti = surfaceArea( r: cylinderRadius, h: cylinderHight)
+
+    
+default:
+    break
+}
+
+
+
+
 // MARK: Functions
+func surfaceArea( r: Double, h: Double) -> Double {
+    return Double.pi * pow(r, 2.0) + Double.pi * pow(r, 2.0) * h
+}
+    
+    
+    
+        
+   
+
+
+let givenLength = 10.0 // seting double using type infrence
+let givenWidth: Double = 2 // set to a double using type anotation
 
 /// Finds the area of a rectangle
 /// - Parameter length: length of rectangle
@@ -26,13 +83,10 @@ func areaOfCircle(radius: Double) -> Double {
     return Double.pi * pow(radius, 2.0)
 }
 
-// MARK: Input
-let givenLength = 10.0 // seting double using type infrence
-let givenWidth: Double = 2 // set to a double using type anotation
+
 
 // MARK: Process
-let area = areaOfRectangle(length: givenLength, width: givenWidth)
+
 
 // MARK: Output
-print("the are is \(area) square units")        // example of "string interpolation"
-                                                //(print the constants as a variable)
+
